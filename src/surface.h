a4acfa44ba0c5914f14737055f7288d65918e987
Richard Stebbing <richie.stebbing@gmail.com>
2014-12-02 13:46:53 +0000

    Add src/surface.h

---
+++ src/surface.h
@@ -0,0 +1,22 @@
+#ifndef SURFACE_H
+#define SURFACE_H
+
+#include <vector>
+
+struct Surface {
+  virtual void M(double* m, const int p, const double* u, const double* const* X) const = 0;
+  virtual void Mu(double* mu, const int p, const double* u, const double* const* X) const = 0;
+  virtual void Mv(double* mv, const int p, const double* u, const double* const* X) const = 0;
+  virtual void Muu(double* muu, const int p, const double* u, const double* const* X) const = 0;
+  virtual void Muv(double* muv, const int p, const double* u, const double* const* X) const = 0;
+  virtual void Mvv(double* mvv, const int p, const double* u, const double* const* X) const = 0;
+  virtual void Mx(double* mx, const int p, const double* u) const = 0;
+
+  virtual int number_of_vertices() const = 0;
+  virtual int number_of_faces() const = 0;
+  virtual int number_of_patches() const = 0;
+  virtual const std::vector<int>& patch_vertex_indices(const int p) const = 0;
+  virtual const std::vector<int>& adjacent_patch_indices(const int p) const = 0;
+};
+
+#endif
